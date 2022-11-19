286af43ae693fe715fe1b427972b54bfeae6cf3a
LightWat <dennislai120@gmail.com>
2022-11-19 13:52:12 +0800

    try

---
+++ demo.py
@@ -0,0 +1,28 @@
+import cv2 as cv
+
+
+def main():
+    # Set up camera
+    capture = cv.VideoCapture(0)
+    if not capture.isOpened():
+        print('Unable to open camera.')
+        exit(0)
+
+    backSub = cv.createBackgroundSubtractorMOG2()
+    while True:
+        ret, frame = capture.read()
+        if frame is None:
+            break
+
+        fgMask = backSub.apply(frame)
+
+        cv.imshow('Frame', frame)
+        cv.imshow('FG Mask', fgMask)
+
+        keyboard = cv.waitKey(30)
+        if keyboard == 'q':
+            break
+
+
+if __name__ == '__main__':
+    main()
