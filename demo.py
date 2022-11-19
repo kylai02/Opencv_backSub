555cb7fb1cee002bd4fdff76ec4ec5a82423f780
LightWat <dennislai120@gmail.com>
2022-11-19 15:57:26 +0800

    test

--- demo.py
+++ demo.py
@@ -53,7 +53,7 @@ def main():
             for i in range(a[0], b[0]):
                 for j in range(a[1], b[1]):
                     sum[button['index']] += fgMask[i][j]
-        
+        print(fgMask[400][10])
         ctr += 1
         chosen = -1
         if ctr >= 24:
